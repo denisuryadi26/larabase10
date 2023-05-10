8dd4a4023fb8dfe6a69ddf05cd6983aae6552a44
Deni Suryadi <deni.w4f@gmail.com>
2023-05-10 14:33:32 +0700

    Update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,127 +1,175 @@
 <!DOCTYPE html>
-<html class="loading" lang="en" data-textdirection="ltr">
-<!-- BEGIN: Head-->
+
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="{{asset('vuexy-bootstrap5/assets/')}}" data-template="vertical-menu-template">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
-    <title>Login Page - Juragan Beku CMS </title>
-    <link rel="apple-touch-icon" href="{{asset('img/icon/favicon.ico')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/icon/favicon.ico')}}">
-    {{-- <link rel="apple-touch-icon" href="{{asset('front/app-assets/images/ico/apple-icon-120.png')}}">--}}
-    {{-- <link rel="shortcut icon" type="image/x-icon" href="{{asset('front/app-assets/images/ico/favicon.ico')}}">--}}
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
+    <title>Login Cover - Pages | Vuexy - Bootstrap Admin Template</title>
 
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/bootstrap-extended.css')}}">
+    <meta name="description" content="" />
 
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/form-validation.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/page-auth.css')}}">
-    <!-- END: Page CSS-->
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{asset('vuexy-bootstrap5/assets/img/favicon/favicon.ico')}}" />
 
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/style.css')}}">
-    <!-- END: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('auth/css/sweetalert2.min.css')}}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/fonts/fontawesome.css')}}" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/fonts/tabler-icons.css')}}" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/fonts/flag-icons.css')}}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../../../public/vuexy-bootstrap5/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../../../public/vuexy-bootstrap5/assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../../../public/vuexy-bootstrap5/assets/css/demo.css" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/css/rtl/core.css')}}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/css/rtl/theme-default.css')}}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/css/demo.css')}}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/libs/node-waves/node-waves.css')}}" />
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/libs/typeahead-js/typeahead.css')}}" />
+    <!-- Vendor -->
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/libs/formvalidation/dist/css/formValidation.min.css')}}" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{asset('vuexy-bootstrap5/assets/vendor/css/pages/page-auth.css')}}" />
+    <!-- Helpers -->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/js/helpers.js')}}"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/js/template-customizer.js')}}"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{asset('vuexy-bootstrap5/assets/js/config.js')}}"></script>
 </head>
-<!-- END: Head-->
 
-<!-- BEGIN: Body-->
+<body>
+    <!-- Content -->
 
-<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
-    <!-- BEGIN: Content-->
-    <div class="app-content content ">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
-            <div class="content-header row">
+    <div class="authentication-wrapper authentication-cover authentication-bg">
+        <div class="authentication-inner row">
+            <!-- /Left Text -->
+            <div class="d-none d-lg-flex col-lg-7 p-0">
+                <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+                    <img src="{{asset('vuexy-bootstrap5/assets/img/illustrations/auth-login-illustration-light.png')}}" alt="auth-login-cover" class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png" data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
+
+                    <img src="{{asset('vuexy-bootstrap5/assets/img/illustrations/bg-shape-image-light.png')}}" alt="auth-login-cover" class="platform-bg" data-app-light-img="illustrations/bg-shape-image-light.png" data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+                </div>
             </div>
-            <div class="content-body">
-                <div class="auth-wrapper auth-v2">
-                    <div class="auth-inner row m-0">
-                        <!-- Brand logo-->
-                        <a class="brand-logo" href="javascript:void(0);">
+            <!-- /Left Text -->
+
+            <!-- Login -->
+            <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+                <div class="w-px-400 mx-auto">
+                    <!-- Logo -->
+                    <div class="app-brand mb-4">
+                        <a href="index.html" class="app-brand-link gap-2">
+                            <span class="app-brand-logo demo">
+                                <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z" fill="#7367F0" />
+                                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
+                                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd" d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z" fill="#7367F0" />
+                                </svg>
+                            </span>
                         </a>
-                        <!-- /Brand logo-->
-                        <!-- Left Text-->
-                        <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-                            <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-                                <img class="img-fluid" src="{{asset('auth/images/login-v2.svg')}}" alt="Login V2" />
+                    </div>
+                    <!-- /Logo -->
+                    <h3 class="mb-1 fw-bold">Welcome to Vuexy! 👋</h3>
+                    <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+                    <form class="auth-login-form mt-2" action="{{route('login')}}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" autofocus />
+                        </div>
+                        <div class="mb-3 form-password-toggle">
+                            <div class="d-flex justify-content-between">
+                                <label class="form-label" for="password">Password</label>
+                                <a href="auth-forgot-password-cover.html">
+                                    <small>Forgot Password?</small>
+                                </a>
+                            </div>
+                            <div class="input-group input-group-merge">
+                                <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                                <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                             </div>
                         </div>
-                        <!-- /Left Text-->
-                        <!-- Login-->
-                        <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
-                            <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-
-                                {{-- <h2 class="card-title font-weight-bold mb-1">Welcome to CMS! 👋</h2>--}}
-
-                                <img class="" alt="logo" style="width:200px !important; height:40px !important" src="{{(!empty($setting['logo_icon']) ? asset('upload/'.$setting['logo_icon']) : asset('vuexy/app-assets/images/logo/logo.png'))}}">
-                                <span style="font-size: 1.7em; font-weight: bold" class="font-weight-bolder">CMS</span>
-                                <p class="card-text mb-2 mt-2">Please sign-in to your account and start the adventure</p>
-                                <form class="auth-login-form mt-2" action="{{route('login')}}" method="POST">
-                                    @csrf
-                                    <div class="form-group">
-                                        <label class="form-label" for="username">Username</label>
-                                        <input class="form-control" id="username" type="text" name="username" placeholder="Username" aria-describedby="username" autofocus="" tabindex="1" required />
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="d-flex justify-content-between">
-                                            <label for="login-password">Password</label><a href="#"><small>Forgot Password?</small></a>
-                                        </div>
-                                        <div class="input-group input-group-merge form-password-toggle">
-                                            <input class="form-control form-control-merge" id="password" type="password" name="password" placeholder="············" aria-describedby="password" tabindex="2" required />
-                                            <div class="input-group-append"><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span></div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input class="custom-control-input" id="remember-me" type="checkbox" tabindex="3" />
-                                            <label class="custom-control-label" for="remember-me"> Remember Me</label>
-                                        </div>
-                                    </div>
-                                    <button class="btn btn-primary btn-block" tabindex="4">Sign in</button>
-                                </form>
-                                <p class="text-center mt-2"><span>Belum punya akun?</span><a href="{{ route('register') }}"><span>&nbsp;Register disini</span></a></p>
+                        <div class="mb-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="remember-me" />
+                                <label class="form-check-label" for="remember-me"> Remember Me </label>
                             </div>
                         </div>
-                        <!-- /Login-->
+                        <button class="btn btn-primary d-grid w-100">Sign in</button>
+                    </form>
+
+                    <p class="text-center">
+                        <span>New on our platform?</span>
+                        <a href="auth-register-cover.html">
+                            <span>Create an account</span>
+                        </a>
+                    </p>
+
+                    <div class="divider my-4">
+                        <div class="divider-text">or</div>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
+                            <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+                        </a>
+
+                        <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
+                            <i class="tf-icons fa-brands fa-google fs-5"></i>
+                        </a>
+
+                        <a href="javascript:;" class="btn btn-icon btn-label-twitter">
+                            <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+                        </a>
                     </div>
                 </div>
             </div>
+            <!-- /Login -->
         </div>
     </div>
-    <!-- END: Content-->
 
+    <!-- / Content -->
 
-    <!-- BEGIN: Vendor JS-->
-    <script src="{{asset('auth/js/vendors.min.js')}}"></script>
-    <!-- BEGIN Vendor JS-->
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/jquery/jquery.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/popper/popper.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/js/bootstrap.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/node-waves/node-waves.js')}}"></script>
 
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="{{asset('auth/js/jquery.validate.min.js')}}"></script>
-    <!-- END: Page Vendor JS-->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/hammer/hammer.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/i18n/i18n.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
 
-    <!-- BEGIN: Theme JS-->
-    <script src="{{asset('auth/js/app-menu.js')}}"></script>
-    <script src="{{asset('auth/js/app.js')}}"></script>
-    <!-- END: Theme JS-->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/js/menu.js')}}"></script>
+    <!-- endbuild -->
 
-    <!-- BEGIN: Page JS-->
-    <script src="{{asset('auth/js/page-auth-login.js')}}"></script>
-    <!-- END: Page JS-->
+    <!-- Vendors JS -->
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')}}"></script>
+    <script src="{{asset('vuexy-bootstrap5/assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')}}"></script>
 
-    <script src="{{asset('lib/sweetalert2/sweetalert2.js')}}"></script>
+    <!-- Main JS -->
+    <script src="{{asset('vuexy-bootstrap5/assets/js/main.js')}}"></script>
 
-
+    <!-- Page JS -->
+    <script src="{{asset('vuexy-bootstrap5/assets/js/pages-auth.js')}}"></script>
 
     @if($message)
     <script>
@@ -138,18 +186,6 @@
         }, 1000);
     </script>
     @endif
-
-    <script>
-        $(window).on('load', function() {
-            if (feather) {
-                feather.replace({
-                    width: 14,
-                    height: 14
-                });
-            }
-        })
-    </script>
 </body>
-<!-- END: Body-->
 
 </html>
